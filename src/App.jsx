dedebbd4a971225df344f86d92695fb20150ea70
sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-05-05 18:58:32 +0530

    Completed this bsic navbar app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Switch } from 'react-router-dom';
-
 import Navbar from './components/Navbar/Navbar'
 import Home from "./components/Home"
 import About from "./components/About"
